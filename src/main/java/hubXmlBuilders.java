502dddbcad1d60301bc15a7d4fc86261408befed
William Spiro <William@Williams-MacBook-Pro.local>
2018-07-09 19:23:30 -0400

    clean up post id

--- src/main/java/hubXmlBuilders.java
+++ src/main/java/hubXmlBuilders.java
@@ -132,10 +132,7 @@ class hubXmlBuilders {
             }
 
             // Build <wp:postIid>
-            Element wpPostId = new Element("post_id", wp);
-            wpPostId.setText(String.valueOf(id + 1));
-            wpPostId.removeAttribute("wp");
-            item.addContent(wpPostId);
+            item.addContent(new Element("post_id", wp).setText(String.valueOf(id + 1)));
 
             // Build <wp:status>
             Element wpStatus = new Element("status", wp);
