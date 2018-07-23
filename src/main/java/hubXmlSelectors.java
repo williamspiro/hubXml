0e8eacc7d246083c3109c12520350fc3e0e63a20
William Spiro <35824747+williamspiro@users.noreply.github.com>
2018-07-23 15:27:24 -0400

    Post body removers

--- src/main/java/hubXmlSelectors.java
+++ src/main/java/hubXmlSelectors.java
@@ -4,13 +4,15 @@ class hubXmlSelectors {
     // VARIABLES TO SET
     // :)
     // Required Elements for a given <item> ff an element is not found in a given post, a default value is set, and the error is logged
-     static final String[] POSTS = {"https://www.kungfupanda.com/dragon-warrior", "https://www.kungfupanda.com/valley-of-peace", "https://www.kungfupanda.com/jade-palace"};
+    static final String[] POSTS = {"https://www.kungfupanda.com/dragon-warrior", "https://www.kungfupanda.com/valley-of-peace", "https://www.kungfupanda.com/jade-palace"};
     static final String TITLE_SELECTOR = "title";
     static final String DATE_SELECTOR = ".published";
     static final String META_DESCRIPTION_SELECTOR = "meta[name=description]";
     static final String AUTHOR_SELECTOR = "a[rel=author]";
     static final String TAGS_SELECTOR = "a[rel=category tag]";
     static final String POST_BODY_SELECTOR = ".post-body";
+    // Optional Element to remove from the post body content (good to use when there is not a post body wrapper)
+    static final String POST_BODY_SELECTOR_REMOVER = ".post-social-sharing-icons";
 
     // Optional elements for a given <item> no default value or error is logged
 
